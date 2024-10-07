218c058885a3c1336cdab0adeb19b9de0f401daa
Fuad <issafuad1818@gmail.com>
2024-10-07 03:17:28 -0700

    saves

--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -5,7 +5,7 @@ const Saved = () => {
   return (
     <div className="bg-[#1d2025] flex justify-start items-center flex-col text-white w-full gap-4 px-3 pt-6 pb-[6.5rem]">
       <h1 className="bg-gradient-naive-orange bg-clip-text text-transparent animate-shimmer bg-[length:200%_200%] text-4xl pb-2">
-        Your Saves
+        Saves
       </h1>
       <div className="grid grid-cols-2 gap-x-3 gap-y-4 w-full">
         {[1, 2, 3, 4, 5, 6, 7, 8].map((data) => {
